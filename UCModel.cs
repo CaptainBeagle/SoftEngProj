67954f2b38f4f3341af8c017d08ab20f9d69a97b
Rhianna Lowery <rhiannalowery@icloud.com>
2023-11-28 12:56:20 -0600

    Corrected some overlooked errors

--- UCModel.cs
+++ UCModel.cs
@@ -25,29 +25,29 @@ namespace SoftEngProj
 
         private void UCModel_Load(object sender, EventArgs e)
         {
-            if (View.theme == 0)
+            if (ComponentModel.savetheme == 0)
             {
                 this.BackColor = SystemColors.Window;
                 Namelb.ForeColor = SystemColors.ControlText;
                 Catlb.ForeColor = SystemColors.ControlText;
                 Locationlb.ForeColor = SystemColors.ControlText;
             }
-            else if (View.theme == 1)
+            else if (ComponentModel.savetheme == 1)
             {
                 this.BackColor = SystemColors.ControlText;
                 Namelb.ForeColor = SystemColors.Control;
                 Catlb.ForeColor = SystemColors.Control;
                 Locationlb.ForeColor = SystemColors.Control;
             }
-            else if (View.theme == 2)
+            else if (ComponentModel.savetheme == 2)
             {
 
             }
-            else if (View.theme == 3)
+            else if (ComponentModel.savetheme == 3)
             {
 
             }
-            else if (View.theme == 4)
+            else if (ComponentModel.savetheme == 4)
             {
 
             }
